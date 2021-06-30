bb5e64e87888e1a03cd7cdc825e720f71a45b62b
rgkenga <86708849+rgkenga@users.noreply.github.com>
2021-06-30 20:31:53 +0500

    Update webook.php

--- webook.php
+++ webook.php
@@ -2,6 +2,6 @@
 header('Access-Control-Allow-Origin: *');
 $headers = "From: from@webhookservesite.ru";
 $message = print_r($_POST,true);
-@mail('anton.strobe@gmail.com', 'Tilda TEST', $message, $headers);
-echo "ok";
+@mail('rgkenga@yandex.ru', 'Tilda TEST', $message, $headers);
+echo "test";
 ?>
