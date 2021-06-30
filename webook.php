a45e1c5b98c810d80b503dac10a1d9c6b2cc26de
rgkenga <86708849+rgkenga@users.noreply.github.com>
2021-06-30 20:40:13 +0500

    Update webook.php

--- webook.php
+++ webook.php
@@ -1,14 +1,9 @@
 <?php
-
 header('Access-Control-Allow-Origin: *');
-
-$headers = "From: rgkenga@yandex.ru";​ 
+$headers = "From: rgkenga@yandex.ru";
 /* замените электронную почту на действующую почту,  с которой вам будет приходить письмо  */
-​
  $message = print_r($_POST,true);
  @mail('rgkenga@yandex.ru', 'Tilda TEST', $message, $headers);
 /* замените электронную почту на вашу действующую почту,  на которую вам будет приходить письмо  */
-
 echo"ok";
-
 ?>
